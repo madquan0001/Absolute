164155b2d4d36b6a687a2ba4d3e4e7558688e912
Quan Maple <quan199877@gmail.com>
2018-10-03 17:37:39 +0700

    Test tìm giá trị tuyệt đối

---
+++ src/com/company/Main.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Absolute {
+
+    public static int findAbsolute(int number){
+        if(number < 0)
+            return  -number;
+        return number;
+    }
+}
